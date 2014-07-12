7c3f56b3d4840a92b9d7946a4047a4f8082728b1
Szabolcs Ban <sab@storesco.com>
2014-07-12 18:18:45 +0100

    helper phpdoc

--- src/app/code/community/Magehack/Autogrid/Helper/Data.php
+++ src/app/code/community/Magehack/Autogrid/Helper/Data.php
@@ -3,11 +3,18 @@
 
 class Magehack_Autogrid_Helper_Data extends Mage_Core_Helper_Abstract
 {
+
+    /**
+     * @return Magehack_Autogrid_Model_Resource_Table
+     */
     public function getCurrentTable()
     {
 
     }
 
+    /**
+     * @return Magehack_Autogrid_Model_Resource_Table_Collection for grid
+     */
     public function getCollection()
     {
 
