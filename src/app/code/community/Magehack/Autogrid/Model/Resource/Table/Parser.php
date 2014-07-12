f24b6f2a83c107fca68d60d3f0f2fb492ade88fc
Szabolcs Ban <sab@storesco.com>
2014-07-12 17:16:11 +0100

    parser cleanup + phpdoc

--- src/app/code/community/Magehack/Autogrid/Model/Resource/Table/Parser.php
+++ src/app/code/community/Magehack/Autogrid/Model/Resource/Table/Parser.php
@@ -5,7 +5,6 @@ class Magehack_Autogrid_Model_Resource_Table_Parser
     implements Magehack_Autogrid_Model_Resource_Table_ParserInterface
 {
 
-    protected $_tableColumns = array();
     protected $_primaryKey = null;
     protected $_cells = array();
     /**
@@ -16,6 +15,10 @@ class Magehack_Autogrid_Model_Resource_Table_Parser
         $this->_init('magehack_autogrid/parser', 'id');
     }
 
+    /**
+     * @param string $tableName
+     * @return $this|void
+     */
     public function init($tableName)
     {
         $ra = $this->_getReadAdapter();
@@ -31,11 +34,17 @@ class Magehack_Autogrid_Model_Resource_Table_Parser
         return $this;
     }
 
+    /**
+     * @return null|string
+     */
     public function getPrimaryKey()
     {
         return $this->_primaryKey;
     }
 
+    /**
+     * @return array|Magehack_Autogrid_Model_Table_Cell
+     */
     public function getTableColumns()
     {
         return $this->_cells;
