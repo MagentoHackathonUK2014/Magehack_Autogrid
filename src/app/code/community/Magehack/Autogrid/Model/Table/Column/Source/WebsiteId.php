6d613b9b0ddb266a7b59532ba308b35dc9aa877f
Vinai Kopp <vinai@netzarbeiter.com>
2014-07-13 11:06:41 +0200

    Add website_id autogrid source model

---
+++ src/app/code/community/Magehack/Autogrid/Model/Table/Column/Source/WebsiteId.php
@@ -0,0 +1,9 @@
+<?php
+
+
+class Magehack_Autogrid_Model_Table_Column_Source_WebsiteId
+    extends Magehack_Autogrid_Model_Table_Column_Source_Abstract
+{
+    protected $_collectoin = 'core/website';
+    protected $_labelAttribute = 'name';
+}
