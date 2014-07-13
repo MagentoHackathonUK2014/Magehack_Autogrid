fd75591445167df38039760e3b91202d23194a27
Vinai Kopp <vinai@netzarbeiter.com>
2014-07-13 11:06:19 +0200

    Add store_id autogrid source model

---
+++ src/app/code/community/Magehack/Autogrid/Model/Table/Column/Source/StoreId.php
@@ -0,0 +1,9 @@
+<?php
+
+
+class Magehack_Autogrid_Model_Table_Column_Source_StoreId
+    extends Magehack_Autogrid_Model_Table_Column_Source_Abstract
+{
+    protected $_collectoin = 'core/store';
+    protected $_labelAttribute = 'name';
+}
