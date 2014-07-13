605d319504b60a94bdf3731a6b1ac007e4d06749
Vinai Kopp <vinai@netzarbeiter.com>
2014-07-13 11:05:39 +0200

    Add entity_type_id autogrid source model

---
+++ src/app/code/community/Magehack/Autogrid/Model/Table/Column/Source/EntityTypeId.php
@@ -0,0 +1,23 @@
+<?php
+
+
+class Magehack_Autogrid_Model_Table_Column_Source_EntityTypeId
+    extends Magehack_Autogrid_Model_Table_Column_Source_Abstract
+{
+    /**
+     * Populate the $_options property
+     *
+     * @return null
+     */
+    function _loadOptions()
+    {
+        $collection = Mage::getResourceModel('eav/entity_type_collection');
+        foreach ($collection as $id => $model) {
+            $this->_options[] = array(
+                'value' => $id,
+                'label' => $model->getData('entity_type_code')
+            );
+        }
+    }
+
+} 
