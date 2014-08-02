45085693688119a12d56a92dabc2e9664fac1ef8
Vinai Kopp <vinai@netzarbeiter.com>
2014-08-02 22:20:44 +0800

    Add admin store to store id source model

--- src/app/code/community/Magehack/Autogrid/Model/Table/Column/Source/StoreId.php
+++ src/app/code/community/Magehack/Autogrid/Model/Table/Column/Source/StoreId.php
@@ -6,4 +6,14 @@ class Magehack_Autogrid_Model_Table_Column_Source_StoreId
 {
     protected $_collection = 'core/store_collection';
     protected $_labelAttribute = 'name';
+
+    protected function _getCollection()
+    {
+        /** @var Mage_Core_Model_Resource_Store_Collection $collection */
+        $collection = parent::_getCollection();
+        $collection->setLoadDefault(true);
+        return $collection;
+    }
+
+
 }
